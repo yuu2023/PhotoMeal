0297bf2f12ab9b86ed42f7dcfc519bb6f6f61f16
yuu2023 <cupth20297@yahoo.co.jp>
2023-06-04 19:38:01 +0900

    ペジネーションのリンクにパラメータを持たせるよう修正

--- resources/views/vendor/pagination/custom.blade.php
+++ resources/views/vendor/pagination/custom.blade.php
@@ -1,4 +1,31 @@
 @if ($paginator->hasPages())
+    <?php
+    $params = "";
+    if(isset($_GET['search'])) {
+        $params .= '&search='.$_GET['search'];
+    }
+
+    if(isset($_GET['filter'])) {
+        $params .= '&filter='.$_GET['filter'];
+    }
+
+    if(isset($_GET['latitude'])) {
+        $params .= '&latitude='.$_GET['latitude'];
+    }
+
+    if(isset($_GET['longitude'])) {
+        $params .= '&longitude='.$_GET['longitude'];
+    }
+
+    if(isset($_GET['sort'])) {
+        $params .= '&sort='.$_GET['sort'];
+    }
+
+    if(isset($_GET['mode'])) {
+        $params .= '&mode='.$_GET['mode'];
+    }
+
+    ?>
     <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex justify-center">
         <div>
             <span class="relative z-0 inline-flex shadow-sm rounded-md">
@@ -12,7 +39,7 @@
                         </span>
                     </span>
                 @else
-                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="{{ __('pagination.previous') }}">
+                    <a href="{{ $paginator->previousPageUrl().$params }}" rel="prev" class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="{{ __('pagination.previous') }}">
                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
                         </svg>
@@ -53,13 +80,13 @@
                             <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-blue-700 bg-blue-100 border border-blue-300 cursor-default leading-5">{{ $i }}</span>
                         </span>
                     @else
-                        <a class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" href="{{ $paginator->url($i) }}">{{ $i }}</a>
+                        <a class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" href="{{ $paginator->url($i).$params }}">{{ $i }}</a>
                     @endif
                 @endfor
 
                 {{-- Next Page Link --}}
                 @if ($paginator->hasMorePages())
-                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="{{ __('pagination.next') }}">
+                    <a href="{{ $paginator->nextPageUrl().$params }}" rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="{{ __('pagination.next') }}">
                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
                         </svg>
